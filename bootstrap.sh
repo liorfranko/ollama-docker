30de8bb44e388f4db886ce0746034d358e34a4d6
Lior Franko <lior.franko@ironsrc.com>
2025-03-16 00:03:31 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ yum install -y git docker
 curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
 systemctl start docker
 systemctl enable docker
-git clone https://github.com/valiantlynx/ollama-docker.git
+git clone https://github.com/liorfranko/ollama-docker.git
 cd ollama-docker
 docker-compose up -d
